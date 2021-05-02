ce090e67fa7ee0f40801cedc6ba81b9121df608a
alextriam <alextriam@gmail.com>
2021-05-02 14:36:08 +0300

    added some styles to adminpage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def index
+    if current_user.is_admin
+      render '/pages/_admin_page'
+    else
+      render '/pages/_user_page'
+    end
   end
 end
