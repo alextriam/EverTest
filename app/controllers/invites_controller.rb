e5f563314ada29753124de797dbffa7f794b3e24
alextriam <alextriam@gmail.com>
2021-05-03 13:42:38 +0300

    added flash message to invites create

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -11,7 +11,8 @@ class InvitesController < ApplicationController
     @invite = User.new(email: params[:email], password: params[:password], password_confirmation: params[:password], level_id: params[:level_id])
 
     if @invite.save
-
+      InviteMailer.with(invite: @invite).invite_email(params[:email], params[:password]).deliver_now
+      flash["alert alert-success"] = "Інвайт успішно створено та відправлено користувачеві"
       redirect_to root_path
     else
       render 'new'
