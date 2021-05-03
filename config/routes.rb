020d82152bd2bdaa4ce8d93cf28ff85d17b71f54
alextriam <alextriam@gmail.com>
2021-05-03 13:35:31 +0300

    added resources tests and solutions

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :invites, only: [:index, :new, :create, :destroy]
+  resources :tests, only: [:index, :show, :new, :create, :destroy]
+  resources :solutions, only: [:index, :show, :new, :create, :destroy]
 end
